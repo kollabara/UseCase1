0cbd4a314a0113adb4b34e24003a8e5546af4c02
SOFTSERVE\skiri <skiri@softserveinc.com>
2023-07-04 13:33:34 +0300

    C#5 UC#1 Create filter by population function

--- UseCase1/Controllers/CountryController.cs
+++ UseCase1/Controllers/CountryController.cs
@@ -7,58 +7,58 @@ namespace  UseCase1.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class CountryController : ControllerBase
-{
+public class CountryController : ControllerBase {
   private readonly HttpClient _client;
 
-  public CountryController(IHttpClientFactory clientFactory)
-  {
+  public CountryController(IHttpClientFactory clientFactory) {
     _client = clientFactory.CreateClient();
   }
 
   [HttpGet]
-  public async Task<IActionResult> Get(string? countryName, long? population, SortOrder? sort, int? firstRows)
-  {
+  public async Task<IActionResult> Get(string? countryName, long? population, SortOrder? sort, int? firstRows) {
     var response = await _client.GetAsync("https://restcountries.com/v3.1/all");
 
-    if (!response.IsSuccessStatusCode)
-    {
-      // Handle error...
+    if (!response.IsSuccessStatusCode) {
+      return StatusCode((int)response.StatusCode);
     }
 
     var jsonString = await response.Content.ReadAsStringAsync();
     var countries = JsonSerializer.Deserialize<List<Country>>(jsonString);
 
-    if (!string.IsNullOrEmpty(countryName))
-    {
+    if (countries is null) {
+      return NotFound();
+    }
+
+    if (!string.IsNullOrEmpty(countryName)) {
       countries = FilterByName(countries, countryName);
     }
 
     // Filter by population
-    if (population.HasValue)
-    {
-      countries = countries.Where(c => c.Population == population.Value).ToList();
+    if (population.HasValue) {
+      countries = FilterByPopulation(countries, population.Value);
     }
 
     // Sort
-    if (sort.HasValue)
-    {
+    if (sort.HasValue) {
       countries = sort == SortOrder.Ascending
         ? countries.OrderBy(c => c.Population).ToList()
         : countries.OrderByDescending(c => c.Population).ToList();
     }
 
     // Select first rows
-    if (firstRows.HasValue)
-    {
+    if (firstRows.HasValue) {
       countries = countries.Take(firstRows.Value).ToList();
     }
 
     return Ok(countries);
   }
-  
-  private static List<Country> FilterByName(IEnumerable<Country> countries, string nameFilter)
-  {
-    return countries.Where(c => c.Name.Common != null && c.Name.Common.IndexOf(nameFilter, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+
+  private static List<Country> FilterByName(IEnumerable<Country> countries, string nameFilter) {
+    return countries.Where(c =>
+      c.Name.Common != null && c.Name.Common.IndexOf(nameFilter, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+  }
+
+  private static List<Country> FilterByPopulation(IEnumerable<Country> countries, long populationLimit) {
+    return countries.Where(c => c.Population < populationLimit * 1_000_000).ToList();
   }
 }
